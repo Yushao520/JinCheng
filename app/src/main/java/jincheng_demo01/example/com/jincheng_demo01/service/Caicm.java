e184e99255403a259b5fb6b549821824cc747129
Mr.µ¼ÑÝ <1003007938@qq.com>
2017-12-30 11:54:39 +0800

    Caicm

--- app/src/main/java/jincheng_demo01/example/com/jincheng_demo01/service/Caicm.java
+++ app/src/main/java/jincheng_demo01/example/com/jincheng_demo01/service/Caicm.java
@@ -1,9 +1,19 @@
 package jincheng_demo01.example.com.jincheng_demo01.service;
 
+import android.util.Log;
+
 /**
  * Created by Mr.导演 on 2017/12/30.
  */
 
 public class Caicm {
+
+    private static final String TAG = "=====";
     private String aa = "你是智障吗";
+
+    public void run(){
+        for (int i = 0; i < 100000000; i++) {
+            Log.i(TAG, "王靖宇" );
+        }
+    }
 }
